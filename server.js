3b0857bf233fe73764cc7452230de6ae602ff72c
andrewhosack <49813742+andrewhosack@users.noreply.github.com>
2020-04-07 10:53:17 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.static('public'));
 
