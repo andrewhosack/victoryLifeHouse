35042e7f8d0bb63a2897e4845506a62d38b82f1a
andrewhosack <49813742+andrewhosack@users.noreply.github.com>
2020-04-06 21:12:40 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-const index = require('./index.js')
+//const index = require('./index.js')
 const express = require('express')
 const app = express();
 const port = 3000
 const path = require('path');
 const router = express.Router();
 
-//router.get('/',function(req,res){
- // res.sendFile(path.join(__dirname+'/index.html'));
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
-//});
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
 });
+
 
 app.use(express.static('build'));
 
