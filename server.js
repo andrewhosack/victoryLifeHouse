8bd28e3dd7b13339fa07cd4dabae6d63dced57a7
andrewhosack <49813742+andrewhosack@users.noreply.github.com>
2020-04-07 06:46:21 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+app.use('public', express.static(path.join(__dirname, "public")));
+
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
 
-app.use('/public', express.static(path.join(__dirname, "public")));
-
 //add the router
-app.use('/', router);
+//app.use('/', router);
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
