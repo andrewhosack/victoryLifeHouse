a949784bd77f5cf3f2ef17c863b9c04393b00743
andrewhosack <49813742+andrewhosack@users.noreply.github.com>
2020-04-06 20:52:21 -0500

    Changing for heroku

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-const db = require('./queries')
+const index = require('./index.js')
 const express = require('express')
 const app = express();
 const port = 3000
 const path = require('path');
 const router = express.Router();
 
-const bodyParser = require('body-parser')
-
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
-});
-
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
-});
-
-router.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
 });
 
 
@@ -25,30 +15,6 @@ app.use(express.static('build'));
 
 //add the router
 app.use('/', router);
-app.listen(process.env.PORT);
+app.listen(process.env.port || port);
 
-console.log('Running at Port ');
-
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
-
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
-app.post('/guests', db.createGuest)
-app.get('/guests/:id', db.getGuestById)
-app.put('/guests/:id', db.updateGuest)
-
-app.listen(process.env.PORT, () => {
-  console.log(`App running on port`)
-})
+console.log('Running at Port 3000');
