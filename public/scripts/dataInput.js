8ee0d781c250edc0f881a745b05856a13ac327e6
andrewhosack <49813742+andrewhosack@users.noreply.github.com>
2020-05-05 10:42:29 -0500

    Add files via upload

--- public/scripts/dataInput.js
+++ public/scripts/dataInput.js
@@ -31,53 +31,30 @@ function loadNavBar() {
 function loadPage() {
 
 //Creating splash screen
-let splashScreenDiv = document.createElement('div');
-splashScreenDiv.setAttribute("id", "splashScreenDiv");
-splashScreenDiv.innerHTML = '<p>Welcome to VictoryLife House!</p>';
-splashScreenDiv.classList.add("sectionHeader");
+let displayGuestsDiv = document.createElement('div');
+displayGuestsDiv.setAttribute("id", "displayGuestsDiv");
+displayGuestsDiv.innerHTML = '<p>Current guests:</p>';
+displayGuestsDiv.classList.add("sectionHeader");
 
-let splashScreenDiv2 = document.createElement('div');
-splashScreenDiv2.setAttribute("id", "splashScreenDiv2");
-splashScreenDiv2.innerHTML = '<img src="Images/VLH.jpg" alt="banner of VLH" width="100%">';
+let displayGuestsDiv2 = document.createElement('div');
+displayGuestsDiv2.setAttribute("id", "displayGuestsDiv2");
+displayGuestsDiv2.innerHTML = 'Hello';
 
-let btnSplashScreen = document.createElement('div');
-btnSplashScreen.setAttribute("id", "btnSplashScreen");
-btnSplashScreen.innerHTML = '<input type="button" value="Let\'s get started! >>" id="btnSplashScreenNext" class = "navButtons">';
-btnSplashScreen.classList.add("divBackAndNext");
-
-let splashScreenTestimonials = document.createElement('div');
-splashScreenTestimonials.setAttribute("id", "splashScreenTestimonials");
-
-splashScreenTestimonials.innerHTML =
-'  <div id="splashScreenTestimonialsHeader" class="sectionHeader"><p>Encouragment from previous success!</p></div><div class="container">' +
-    '<img src="Images/bandmember.jpg" alt="Avatar" style="width:90px">' +
-    '<p><span>Chris Fox.</span> Volunteer Money Management Instructor</p>' +
-    '<p>"I\'ve been volunteering at VictoryLife House for five years. I appreciate what they are doing for the community, and I\'m always ready to come back."</p>' +
-  '</div>' +
-  '<div class="container">' +
-    '<img src="Images/avatar_g2.jpg" alt="Avatar" style="width:90px">' +
-    '<p><span>Rebecca Flex.</span> Loving daughter and mother of three</p>' +
-    '<p>"Before VictoryLife House, I didn\'t have a direction. Although I still face a tough reality, I feel more equipped to move forward!"</p>' +
-  '</div>';
-  splashScreenTestimonials.classList.add('splashScreenTestimonials');
+// //creating the footer
+// let divfooter = document.createElement('div');
+// divfooter.setAttribute("id", "divfooter");
+// //divfooter.innerHTML = '<div class="footer"> <div class="footerColumn"></div><div class="footerColumn"><div> About </div> <div>Login</div>   </div> <div class="footerColumn"> <div> Long-term Application </div> <div> Short-term Application </div>  <div> Print Forms </div>      </div>   </div>'
+// divfooter.innerHTML = '<ul class="footerColumn"> <li><a href="#" class="anchor">Home</a></li> <li><a href="#" class="anchor">Data Input</a></li> <li><a href="#" class="anchor">Log in</a></li> </ul>';
+// divfooter.classList.add("divQuestionContent");
 
 
-
-//creating the footer
-let divfooter = document.createElement('div');
-divfooter.setAttribute("id", "divfooter");
-//divfooter.innerHTML = '<div class="footer"> <div class="footerColumn"></div><div class="footerColumn"><div> About </div> <div>Login</div>   </div> <div class="footerColumn"> <div> Long-term Application </div> <div> Short-term Application </div>  <div> Print Forms </div>      </div>   </div>'
-divfooter.innerHTML = '<ul class="footerColumn"> <li><a href="#" class="anchor">Home</a></li> <li><a href="#" class="anchor">Data Input</a></li> <li><a href="#" class="anchor">Log in</a></li> </ul>';
-divfooter.classList.add("divQuestionContent");
 
 //Appending the divs to the various sections
-var spashScreen = document.getElementById('displayGuests');
-spashScreen.appendChild(splashScreenDiv);
-spashScreen.appendChild(splashScreenDiv2);
-spashScreen.appendChild(btnSplashScreen);
-spashScreen.appendChild(splashScreenTestimonials);
+var displayGuests = document.getElementById('displayGuests');
+displayGuests.appendChild(displayGuestsDiv);
+displayGuests.appendChild(displayGuestsDiv2);
 
-var footer = document.getElementById('footer');
+//var footer = document.getElementById('footer');
 //footer.appendChild(divfooter);
 
 }
@@ -112,7 +89,7 @@ for (var i=0; i<anchors.length; i++) {
 function handler() {
   //if navbar, Home is selected
 if(this.innerHTML === 'Home') {
-  clearFormTextboxes();
+  window.location.replace("/");
   showClass('splashScreen');
   hideClass('basicInfo');
   hideClass('addressInfo');
@@ -121,7 +98,6 @@ if(this.innerHTML === 'Home') {
   hideClass('familyAndFriends');
   hideClass('generalInfo');
   hideClass('summaryPage');
-  window.location.replace("/");
 }
 
 if(this.innerHTML === 'Data Input') {
